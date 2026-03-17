dd59e7026500ad2689a7e89614b2192672ac9c55
Guillaume Plum <guiplum@gmail.com>
2026-03-17 09:32:00 +0100

    feat: a few headers for linux

---
+++ RedAlert/linuxlib/memflag.h
@@ -0,0 +1,4 @@
+inline void *operator new(size_t size, MemoryFlagType flag) { return (Alloc(size, flag)); }
+inline void *operator new[](size_t size, MemoryFlagType flag) { return (Alloc(size, flag)); }
+
+inline void *Add_Long_To_Pointer(void const *ptr, long size) { return ((void *)((char const *)ptr + size)); }
