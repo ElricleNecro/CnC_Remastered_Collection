6d7569859819934ad1357e7e993bdacd5615a0d7
Guillaume Plum <guiplum@gmail.com>
2026-05-06 11:40:14 +0200

    fix: remove deprecated bool

--- RedAlert/keyboard.h
+++ RedAlert/keyboard.h
@@ -33,20 +33,7 @@
 #ifndef KEYBOARD_H
 #define KEYBOARD_H
 
-#include <windows.h>
-
-/*
-**	The "bool" integral type was defined by the C++ comittee in
-**	November of '94. Until the compiler supports this, use the following
-**	definition.
-*/
-#ifndef __BORLANDC__
-#ifndef TRUE_FALSE_DEFINED
-#define TRUE_FALSE_DEFINED
-enum { false = 0, true = 1 };
-typedef int bool;
-#endif
-#endif
+#include "utils.h"
 
 typedef enum {
 	WWKEY_SHIFT_BIT = 0x100,
